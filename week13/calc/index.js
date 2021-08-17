18c06ca68cad67c7f903a0193035eb4f0181876a
it-kirs <tgavrilova55@gmail.com>
2021-08-17 21:52:37 +0300

    week13

---
+++ week13/calc/index.js
@@ -0,0 +1,20 @@
+function sum() {
+    let a = prompt("Введите 1-е число:");
+    let b = prompt("Введите 2-е число:");
+    alert (Number(a)+Number(b));
+}
+function sub() {
+    let a = prompt("Введите 1-е число:");
+    let b = prompt("Введите 2-е число:");
+    alert (Number(a)-Number(b));
+}
+function multiply() {
+    let a = prompt("Введите 1-е число:");
+    let b = prompt("Введите 2-е число:");
+    alert (Number(a)*Number(b));
+}
+function divide() {
+    let a = prompt("Введите 1-е число:");
+    let b = prompt("Введите 2-е число:");
+    alert (Number(a)/Number(b));
+}
