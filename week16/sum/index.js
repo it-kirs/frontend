73754f57ed6cedb2fb63c3fca34ed1f01ebbf870
it-kirs <tgavrilova55@gmail.com>
2021-09-30 18:58:18 +0300

    change sum

--- week16/sum/index.js
+++ week16/sum/index.js
@@ -1,19 +1,19 @@
 function sumInput(){
-    let numbers=[];
-    let num=1;
-    while (num!="" && num!=null && isFinite(num)){
-      num=prompt("Введите числовые значения:");
-      if (num=="" || num==null || !isFinite(num)) break;
-      numbers.push(+num);
-    }
-    numbers.sort(function(a, b){return a - b});
-  alert(numbers);
-    let sum=0;
-    for (num of numbers)
-    {
-      sum+=num
-    }
-    alert(sum);
+  let numbers=[];
+  let num=1;
+  while (true){
+    num=prompt("Введите числовые значения:");
+    if ((num!=null&&num.trim()=="") || num=="" || num==null || !isFinite(num)) break;
+    numbers.push(+num);
   }
-  sumInput();
+  numbers.sort(function(a, b){return a - b});
+alert(numbers);
+  let sum=0;
+  for (num of numbers)
+  {
+    sum+=num
+  }
+  alert(sum);
+}
+sumInput();
   
