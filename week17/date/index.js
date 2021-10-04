e551def85829c4cacd4298838bf00d64165ab218
it-kirs <tgavrilova55@gmail.com>
2021-10-04 22:30:09 +0300

    add week17 date

--- week17/date/index.js
+++ week17/date/index.js
@@ -1,9 +1,28 @@
-function formatDate() {
-    
-    let date = new Date(document.getElementById('userdate').value);
-    let x = new Date() - date;
-
-  if (x< 1000) { 
+function formatDate(date) {
+  let result = new Date()-date;
+  if (result < 1000) { 
     return "прямо сейчас";
-}
-alert( formatDate(new Date(new Date - 1)) ); 
+  }
+  else if (result < 60000) {
+    return Math.round(result/1000) + " сек. назад";
+  }
+  else if (result < 3600000) {
+    return Math.round(result/60000) + " мин. назад";
+  }
+  let day=("0"+date.getDate()).toString().slice(-2);
+  let month=("0"+(date.getMonth()+1)).toString().slice(-2);
+  let year=(date.getFullYear()).toString().slice(-2);
+  let hour=("0"+date.getHours()).toString().slice(-2);
+  let minutes=("0"+date.getMinutes()).toString().slice(-2);
+  
+  return `${day}.${month}.${year} ${hour}:${minutes}`;
+  }
+  
+alert( formatDate(new Date(new Date - 1)) ); // "прямо сейчас"
+
+alert( formatDate(new Date(new Date - 30 * 1000)) ); // "30 сек. назад"
+
+alert( formatDate(new Date(new Date - 5 * 60 * 1000)) ); // "5 мин. назад"
+
+// дата 4 дня назад
+alert( formatDate(new Date(new Date - 86400* 4 * 1000)) );
