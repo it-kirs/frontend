faa9189021d5d5f4fc74baa5428548efa47b2626
it-kirs <tgavrilova55@gmail.com>
2021-10-16 21:55:14 +0300

    comment

--- week18/comment/index.js
+++ week18/comment/index.js
@@ -1,3 +1,4 @@
+const ul = document.querySelector('ul');
 document.addEventListener("DOMContentLoaded", function (event) {
     let name= localStorage.getItem('name');
     if (name!=null) {
@@ -8,14 +9,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
     if (base64String!=null) {
         document.getElementById("img_avatar").src = "data:image/png;base64," + base64String;
     }
-    /*let text= localStorage.getItem('comment');
-    if (text!=null) {
-     document.getElementById('comment1').innerHTML=text;
-     
-    }*/
-    let text = JSON.parse(localStorage.getItem('comment')); 
-    
-    alert(text);
+    let arr;
+if (localStorage.getItem('comment')) {
+  arr = JSON.parse(localStorage.getItem('comment'))
+} else {
+  arr = []
+}
+localStorage.setItem('comment', JSON.stringify(arr));
 }); 
 let arr = [];
 let i=0;
@@ -27,21 +27,31 @@ function comment() {
     checkSpam(text);
     i=arr.length;
     arr[i]=`${author}: ${cleantext}`;
-    let div = document.createElement('div');
-    div.setAttribute("id", i);  
+    //let div = document.createElement('div');
+    //div.setAttribute("id", i);  
     document.getElementById('textcomment').value='';
-    div.innerHTML =arr[i];
-    document.getElementById('comment1').appendChild(div);
+    //div.innerHTML =arr[i];
+    //document.getElementById('comment1').appendChild(div);
+    createLi(arr[i]);
     if (localStorage.getItem('name')==null){
         localStorage.setItem('name',author);
     }
-    
-    localStorage.setItem('comment',localStorage.getItem('comment')+JSON.stringify(arr[i]));
+    //localStorage.setItem('comment',localStorage.getItem('comment')+JSON.stringify(arr[i]));
+    localStorage.setItem('comment',JSON.stringify(arr));
     
 }
 function checkSpam(text) {
     cleantext=text.replace(/xxx|viagra/gi, "***");}
-
+    const data = JSON.parse(localStorage.getItem('comment'));
+    
+    data.forEach(function(comment) {
+        createLi(comment);
+    });
+    function createLi (commenttext){
+        let elemli=document.createElement('li');
+        elemli.textContent = commenttext;
+        ul.appendChild(elemli);
+    }
     //Вставка аватарки
 document.getElementById('file').addEventListener('change', (e) => {
     const file = e.target.files[0];
