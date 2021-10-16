0ef5052fb4ebac0272d3b57a5b18753d0e314c00
it-kirs <tgavrilova55@gmail.com>
2021-10-16 22:09:28 +0300

    cmmt

--- week18/comment/index.js
+++ week18/comment/index.js
@@ -1,4 +1,11 @@
 const ul = document.querySelector('ul');
+let arr;
+if (localStorage.getItem('comment')) {
+  arr = JSON.parse(localStorage.getItem('comment'))
+} else {
+  arr = []
+}
+localStorage.setItem('comment', JSON.stringify(arr));
 document.addEventListener("DOMContentLoaded", function (event) {
     let name= localStorage.getItem('name');
     if (name!=null) {
@@ -9,15 +16,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
     if (base64String!=null) {
         document.getElementById("img_avatar").src = "data:image/png;base64," + base64String;
     }
-    let arr;
-if (localStorage.getItem('comment')) {
-  arr = JSON.parse(localStorage.getItem('comment'))
-} else {
-  arr = []
-}
-localStorage.setItem('comment', JSON.stringify(arr));
+    
 }); 
-let arr = [];
 let i=0;
 let text;
 function comment() {
@@ -27,23 +27,18 @@ function comment() {
     checkSpam(text);
     i=arr.length;
     arr[i]=`${author}: ${cleantext}`;
-    //let div = document.createElement('div');
-    //div.setAttribute("id", i);  
     document.getElementById('textcomment').value='';
-    //div.innerHTML =arr[i];
-    //document.getElementById('comment1').appendChild(div);
     createLi(arr[i]);
     if (localStorage.getItem('name')==null){
         localStorage.setItem('name',author);
     }
-    //localStorage.setItem('comment',localStorage.getItem('comment')+JSON.stringify(arr[i]));
     localStorage.setItem('comment',JSON.stringify(arr));
     
 }
 function checkSpam(text) {
     cleantext=text.replace(/xxx|viagra/gi, "***");}
+
     const data = JSON.parse(localStorage.getItem('comment'));
-    
     data.forEach(function(comment) {
         createLi(comment);
     });
