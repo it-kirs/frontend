cd7eff35edc123872bd726560e3bfab17d453b50
it-kirs <tgavrilova55@gmail.com>
2021-10-24 19:28:54 +0300

    site

--- week20/site/index.js
+++ week20/site/index.js
@@ -4,19 +4,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 function getAPOD(){
     fetch('https://api.nasa.gov/planetary/apod?api_key=hZYaPaql7iB82dcsKexVisWxYYJn8x2nZPUGOAmJ')
-    .then(response =>response.json()
+    .then(response =>response.json())
     .then(apod =>{
         document.getElementById("explanation").innerText=apod.explanation;
         document.getElementById("img1").src=apod.url;
         document.getElementById("title").innerText=apod.title;
-    }))
+    })
+    .catch(err => console.log(err));
 }
 function getNeo(startDate,endDate){
     startDate=''
     endDate=''
     fetch('https://api.nasa.gov/neo/rest/v1/feed?start_date='+startDate+'&end_date='+endDate+'&api_key=hZYaPaql7iB82dcsKexVisWxYYJn8x2nZPUGOAmJ') 
-    .then(response =>response.json()
+    .then(response =>response.json())
     .then(neo=>{
         document.getElementById("count").innerText='Counts of near earth Asteroids today: '+ neo.element_count; 
-}))
+})
+.catch(err => console.log(err));
 }
