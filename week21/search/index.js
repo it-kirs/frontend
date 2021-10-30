8b1bf4980fb592e7afd703aa88cffc9f6b64a9d5
it-kirs <tgavrilova55@gmail.com>
2021-10-30 17:28:06 +0300

    add search

--- week21/search/index.js
+++ week21/search/index.js
@@ -1,16 +1,14 @@
 function search() {
     let phrase=document.getElementById("search").value;
-    //fetch("https://api.giphy.com/v1/gifs/search?api_key=IuZJBI8JmPrHBOiRTyc7wAt4coqXaKli&q="+phrase+"&limit=5&offset=0&rating=g&lang=en",
-    fetch("https://api.giphy.com/v1/gifs/search?api_key=IuZJBI8JmPrHBOiRTyc7wAt4coqXaKli&q=cat&limit=1&offset=0&rating=g&lang=en",
-    /*{headers: {
-        'Access-Control-Allow-Origin':'*'
-      }}*/
+    fetch("https://api.giphy.com/v1/gifs/search?api_key=IuZJBI8JmPrHBOiRTyc7wAt4coqXaKli&q="+phrase+"&limit=5&offset=0&rating=g&lang=en",
     )
     .then(response=>response.json())
     .then(gif=>{
-        //console.log(gif.data[0].url)
-        document.getElementById("gif").src=gif.data[1].url;
-        
+        for (let i=0; i<5; i++) {
+        let newGif=document.createElement("img");
+        newGif.setAttribute("src",gif.data[i].images.original.url);
+        document.getElementById("mygif").appendChild(newGif);    
+        }
     })
     .catch(err => console.log(err));
 }
