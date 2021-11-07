ef8a4ec259d1164f6db3ea1e41fccea101de21d3
it-kirs <tgavrilova55@gmail.com>
2021-11-07 22:15:57 +0300

    add dashboard

--- npm/index.js
+++
@@ -1,5 +0,0 @@
-const moment = require('moment');
-let date=moment();
-console.log(date);
-let day=moment("20211101", "YYYYMMDD").format('dddd');
-console.log(day);
--- week22/dashboard/index.js
+++ week22/dashboard/index.js
@@ -1,4 +1,4 @@
-const chart = require('chart.js');
+const chartjs = require('chart.js');
 let ctx = document.getElementById('myChart').getContext('2d');
 let chart = new Chart(ctx, {
 // Тип графика
@@ -7,36 +7,76 @@ type: 'line',
 // Создание графиков
 data: {
     // Точки графиков
-    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
     // График
     datasets: [{
-        label: 'Weekly dashbord', // Название
-        backgroundColor: 'rgb(255, 99, 132)', // Цвет закраски
-        borderColor: 'rgb(255, 99, 132)', // Цвет линии
-        data: [1, 2, 5, 5, 8, 1, 0] // Данные каждой точки графика
+        label: 'Total tasks', // Название
+        backgroundColor: 'rgb(135, 170, 170)', // Цвет заливки
+        borderColor: 'rgb(135, 170, 170)', // Цвет линии
+        data: [10, 9, 19, 12, 8] // Данные каждой точки графика
     }]
 },
- 
 // Настройки графиков
 options: {}
 });
 
+const anychartconst = require('anychart');
+anychart.onDocumentReady(function () {
 
-anychart.onDocumentLoad(function () {
-    // create an instance of a pie chart
-    var chart = anychart.pie();
-    // set the data
-    chart.data([
-      ["Chocolate", 5],
-      ["Rhubarb compote", 2],
-      ["Crêpe Suzette", 2],
-      ["American blueberry", 2],
-      ["Buttermilk", 1]
-    ]);
-    // set chart title
-    chart.title("Top 5 pancake fillings");
-    // set the container element 
-    chart.container("container");
-    // initiate chart display
-    chart.draw();
-  });
+  // JSON data
+  var json = {
+    // chart settings
+    "chart": {
+      "type": "column",
+      "title": "Weekly activity",
+      // series settings
+      "series": [{
+// New Tasks
+"name": "New",
+"color": "rgb(255, 191, 134)",
+"data": [
+  {"x": "Monday", "value": "1"},
+  {"x": "Tuesday", "value": "2"},
+  {"x": "Wednesday", "value": "5"},
+  {"x": "Thursday", "value": "2"},
+  {"x": "Friday", "value": "1"}
+]
+      },{
+// In Progress Tasks
+"name": "In Progress",
+"color": "rgb(246, 234, 190)",
+"data": [
+  {"x": "Monday", "value": "7"},
+  {"x": "Tuesday", "value": "4"},
+  {"x": "Wednesday", "value": "7"},
+  {"x": "Thursday", "value": "5"},
+  {"x": "Friday", "value": "3"}
+]
+      },
+                 {
+// Done Tasks
+"name": "Done",
+"color": "rgb(200, 227, 212)",
+"data": [
+  {"x": "Monday", "value": "2"},
+  {"x": "Tuesday", "value": "3"},
+  {"x": "Wednesday", "value": "7"},
+  {"x": "Thursday", "value": "5"},
+  {"x": "Friday", "value": "4"}
+]
+      }
+     ],
+      "container": "container"
+    }
+  };
+
+  var chart = anychart.fromJson(json);
+  chart.legend(true);
+  chart.draw();
+});
+
+const moment = require('moment');
+let now = moment();
+moment.locale('ru');
+console.log(now.format('dddd, MMMM DD YYYY, h:mm:ss'));
+document.getElementById('date').innerHTML=now.format('dddd, MMMM DD YYYY');
